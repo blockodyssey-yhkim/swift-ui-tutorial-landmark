65192f36129150c7bfc4282bcc0f8d43889492b8
Youngho Kim <76192395+blockodyssey-yhkim@users.noreply.github.com>
2022-10-31 22:19:24 +0900

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by 김영호 on 2022/10/31.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
